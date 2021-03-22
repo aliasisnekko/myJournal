03d11d7e593db011768dc60dd83dd7e1fa861c43
Nekko Street <clayvon.street@gmail.com>
2021-03-22 09:20:08 -0700

    Initial Upload

--- app.js
+++ app.js
@@ -1,29 +1,62 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require('mongoose');
+let ObjectId = require('mongodb').ObjectID;
 
 const { static } = require("express");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae.  Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Hello! You have now entered the awesome zone. Everything that you once thought real will be proven outright blasphemy after you journey through the page that is Nekko's mind.";
+const aboutContent = "Welcome! This site is used as Nekko's personal diary, read at your own peril.";
+
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public/"));
+//Initializing and creating Database before creating collections inside said DB
+mongoose.connect('mongodb://localhost:27017/postsDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Database Connected');
+});
+
+const postsSchema = new mongoose.Schema({
+    name: String
+});
+
+const Post = mongoose.model('Post', postsSchema);
 
 let posts = [];
+
 
 app.get('/', function(req, res) {
 
     res.render('home', { startingContent: homeStartingContent, posts: posts })
 
+
+});
+
+app.get('/compose', function(req, res) {
+    res.render('compose')
+});
+
+
+app.post('/compose', function(req, res) {
+
+    const post = {
+            title: req.body.postTitle,
+            content: req.body.postBody
+        } //good job making your first js object and remebering how to push to an array
+
+    posts.push(post);
+    res.redirect("/");
 
 });
 
@@ -34,24 +67,16 @@ app.get('/about', function(req, res) {
 });
 
 app.get('/contact', function(req, res) {
-
+    const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae.  Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
     res.render('contact', { contactContent: contactContent })
 
 });
 
-app.get('/compose', function(req, res) {
-    res.render("compose");
-});
 
+app.get('/post/', function(req, res) {
 
-app.post('/compose', function(req, res) {
+    res.render('post', { posts: posts })
 
-    let post = {
-            title: req.body.postTitle,
-            content: req.body.postBody
-        } //good job making your first js object and remebering how to push to an array
-    posts.push(post);
-    res.redirect("/");
 
 });
 
@@ -63,13 +88,15 @@ app.get("/posts/:postName", function(req, res) {
         const storedTitle = _.lowerCase(post.title);
 
         if (storedTitle === requestedTitle) {
-            console.log("Match Found!");
-        } else {
-            console.log("No Match Found")
+            res.render('post', {
+                title: post.title,
+                content: post.content
+            });
+
         }
     });
 });
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is up and running boss!")
 });
